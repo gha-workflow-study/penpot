54007170db5a39fbc1deb01be0128e66ef7b2f7c
Andrey Antukh <niwi@niwi.nz>
2026-02-09 19:14:14 +0100

    :sparkles: Make the colors-to-tokens plugin work on subpath

--- plugins/apps/colors-to-tokens-plugin/src/app/app.config.ts
+++ plugins/apps/colors-to-tokens-plugin/src/app/app.config.ts
@@ -1,6 +1,8 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter([])],
+  providers: [
+    provideRouter([], withHashLocation())
+  ],
 };
